2c8cdadde9065f6e5b69710c996aa50f889c5dd9
Pedro Ribeiro <pedroribeiro64@outlook.com>
2025-04-23 17:02:31 -0300

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Service running on port 3000');
+});
+
+
