4db0f5e81535ce956ba43a7d57a35a30eb5a1d1a
Pedro Ribeiro <pedroribeiro64@outlook.com>
2025-04-24 16:27:48 -0300

    lint: eslint fixes

--- src/utils/http-error.ts
+++ src/utils/http-error.ts
@@ -1,15 +1,14 @@
 export class HttpError extends Error {
-    statusCode: number;
-    additionalData: any;
-  
-    constructor(statusCode: number, message: string, additionalData: any = {}) {
-      super(message);
-      this.statusCode = statusCode;
-      this.additionalData = additionalData;
-    }
+  statusCode: number;
+  additionalData: any;
+
+  constructor(statusCode: number, message: string, additionalData: any = {}) {
+    super(message);
+    this.statusCode = statusCode;
+    this.additionalData = additionalData;
   }
-  
-  export function createHttpError(statusCode: number, message: string, additionalData: any = {}) {
-    return new HttpError(statusCode, message, additionalData);
-  }
-  
+}
+
+export function createHttpError(statusCode: number, message: string, additionalData: any = {}) {
+  return new HttpError(statusCode, message, additionalData);
+}
