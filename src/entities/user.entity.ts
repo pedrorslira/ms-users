ab8adc89597a66b7dfd91c3b4120d78e1ede8ca0
Pedro Ribeiro <pedroribeiro64@outlook.com>
2025-04-23 22:31:25 -0300

    chore: postgres connection

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @Column({ nullable: true })
+  phone?: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import routes from './routes';
+import { AppDataSource } from './config/data-source';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+const port = process.env.PORT || 3000
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Service running on port 3000');
-});
+app.use(express.json());
+app.use(routes);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Successfully connected to database');
+    
+    app.listen(port, () => {
+      console.log(`Service running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to database', err);
+  });
 
 
