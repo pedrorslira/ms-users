bd603a04a1c5d7ab06fe680527bd787efc922f13
Pedro Ribeiro <pedroribeiro64@outlook.com>
2025-04-24 11:30:11 -0300

    chore: added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import routes from './routes';
 import { AppDataSource } from './config/data-source';
 import { errorHandler } from './middlewares/error.middleware';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -10,13 +11,14 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.use(express.json());
+app.use(cors())
 app.use('/msusers', routes);
 app.use(errorHandler);
 
 AppDataSource.initialize()
   .then(() => {
     console.log('Successfully connected to database');
-    
+
     app.listen(port, () => {
       console.log(`Service running on port ${port}`);
     });
